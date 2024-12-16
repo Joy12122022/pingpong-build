41722496f2a1d848352458d9203eee1a73225f07
babyRess <139874554+babyRess@users.noreply.github.com>
2024-12-16 11:10:57 +0700

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-System.register(["./application.bb3a7.js"], function (_export, _context) {
+System.register(["./application.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -6,8 +6,8 @@ System.register(["./application.bb3a7.js"], function (_export, _context) {
     return System["import"](url);
   }
   return {
-    setters: [function (_applicationBb3a7Js) {
-      Application = _applicationBb3a7Js.Application;
+    setters: [function (_applicationJs) {
+      Application = _applicationJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
